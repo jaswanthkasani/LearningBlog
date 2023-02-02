bef588b6541317f560f4d2c1b6ea70d72378e4d7
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-01 22:29:53 -0600

    Create typecasting.java

---
+++ program/typecasting.java
@@ -0,0 +1,15 @@
+class dataTypeCasting{
+  public static void main(String []args){
+    byte b;
+    int i = 12;
+    float f = 1.23;
+    double d = 323.142;
+    
+    i = (int)d; // conversion from double to int
+    b = (byte)d; // from double to byte
+    System.out.println("d =" + d + "i = " + i);
+  }
+}
+                          
+                          
+                         
