0bab3e0d54a2b98639ef2e256c6c1706fd5d0ddd
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-08 23:42:08 -0600

    Create whileloop.java

---
+++ program/control Statements/whileloop.java
@@ -0,0 +1,11 @@
+public class while{
+  Public static void main(String []args){
+    int x = 1;
+    
+    while(x<=10) //Executes the statement as long as (x<=10)
+    {
+      System.out.println(x);
+      x++; //increment x by 1
+    }
+  }
+}
