f2cc4a62f7759560eabc8a4741ab56abf11ccffc
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-08 23:58:01 -0600

    Create parameterizedconstructors.java

---
+++ program/parameterizedconstructors.java
@@ -0,0 +1,12 @@
+class pconstructor{
+  int age;
+  String name;
+  pconstructor(int years, string name1){
+    age = years;
+    name = name1;
+    System.out.println(age +" "+name);
+  }
+  public static void main(String []args){
+    pconstructor p1 - new pconstructor(23, "jash");
+  }
+}
