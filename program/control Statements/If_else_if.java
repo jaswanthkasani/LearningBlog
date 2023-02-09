b99a79f0d52f18e1ea4564f11aab2bdc1556eed1
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-08 23:26:12 -0600

    Create If_else_if.java

---
+++ program/control Statements/If_else_if.java
@@ -0,0 +1,13 @@
+public class IfElseIf{
+Public static void main(String []args){
+  int x = 3;
+  if(x==3){
+    System.out.println("x is 3");
+  }
+  else if (x==2){ 
+    System.out.println("x is 2");
+  }
+  else
+  {
+    System.out.println("x is 3");
+  }
