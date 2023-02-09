6825fbe65d129d2ec5aabb197295ecc02f8d6891
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-08 23:46:19 -0600

    Create switch.java

---
+++ program/control Statements/switch.java
@@ -0,0 +1,12 @@
+public class switch{
+    Public static void main(String []args){
+      int x = 3;
+      switch (x){
+          Case 3: System.out.println("x is 3");
+          Case 2: System.out.println("x is 2");
+          Case 1: System.out.println("x is 1");
+        default: Sytems.out.println("x is not");
+      }
+    }
+}
+          
