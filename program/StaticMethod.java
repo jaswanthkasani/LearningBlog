4feff91e667cfaafc99563a30d6520a3160ae226
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-14 19:44:18 -0600

    Create StaticMethod.java

---
+++ program/StaticMethod.java
@@ -0,0 +1,30 @@
+Class Student{
+  int rollNo;
+  String name;
+  
+Static String college = "SLU";
+
+Static void change(){
+  college = "Saint Louis University";
+}
+  
+Student(int id, string n){
+  rollNo = id;
+  name = n;
+}
+
+void display(){
+  System.out.println(rollNo +" "+name +""+ college);
+}
+}
+
+Public class TestStaticMethod{
+  Public Static void main(String []args){
+    Student.change();
+    Student s1 = new Student(111, JK);
+    Student s2 = new Student(112, Kasani);
+    s1.display();
+    s2.display();
+    
+  }
+}
