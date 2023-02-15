35ab530e3dc0d834d5252a74527f8eab262df6b4
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-15 14:42:53 -0600

    Update this_keyword.java

--- program/this_keyword.java
+++ program/this_keyword.java
@@ -7,7 +7,7 @@ Student(int age){
   this.age = age;
 }
 
-Public static void main(String []args){
+public static void main(String []args){
   Student s1 = new Student(); //Student() constructor will be invoked
   Student s2 = new Student(25);//Student(int age) constructor will be invoked
 }
