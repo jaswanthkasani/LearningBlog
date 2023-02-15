2232e3f65cbab1b15862fea495aca98576852882
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-14 18:49:02 -0600

    Create methodOverloading.java

---
+++ program/methodOverloading.java
@@ -0,0 +1,19 @@
+class Adder{
+  public int add(int x, int y){
+    return x + y;
+  }
+  
+  public double add(int x, double y){
+    return x + y;
+  }
+  
+public class Test{
+  Public static void main(String []args){
+    Test t1 = new Test();
+    int b = 32;
+    int c= 3;
+    int result = t1.add(b, c); 
+    double doubleResult = t1,add(22, 9.3);
+  }
+}
+  
