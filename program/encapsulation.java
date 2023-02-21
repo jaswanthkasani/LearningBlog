b32fc4f7e501e16b147adfccd050f7f7bbff560c
jaswanthkasani <89626086+jaswanthkasani@users.noreply.github.com>
2023-02-21 15:39:56 -0600

    Update encapsulation.java

--- program/encapsulation.java
+++ program/encapsulation.java
@@ -14,3 +14,23 @@ class Test{
     E.setA(12);
   }
 }
+
+
+//without Encapsulation
+
+class Foo{
+  public int a;
+  public int b;
+public void setA(int input){
+  a = input;
+  b = a/3;
+  
+class Test{
+  public static void main(String []args){
+    Foo f = new Foo();
+    f.a = 1;
+    f.b = 2;
+  }
+}
+
+//Since the variables are public, they can be accessed outside the class
